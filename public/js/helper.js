14ca4130e35865d942bbe6d5ef372c125b5b64bc
Sinisa Ristic <sinisa.ristic@gmail.com>
2015-11-18 11:24:47 +0100

    Sredjen java script.

--- public/js/helper.js
+++ public/js/helper.js
@@ -1,14 +1,47 @@
+/**
+ * Called when the document load is completed.
+ * @param {type} param
+ */
 $(document).ready(function() {
-   fillContent();
-   $('#definition_id').on('change', function(evt) {
-         fillContent();        
+    // Definition id from entity definitions.
+    fill('#definition_id');
+    $('#definition_id').on('change load', function(evt) {
+       fill('#definition_id');
+    });
+    
+    // Client id from clients.
+    fill('#client_id');        
+    $('#client_id').on('change load', function(evt) {
+        fill('#client_id');
     }); 
 });
 
-var fillContent = function() {
-//    var text = $('select option:selected').text();
-    var id = $('select option:selected').val();
-    $.get('/pms/entity-definition/preview/' + id, function(data) {
-        $('.attribute-content').html(data); 
-    });
-};
+/**
+ * Function which calls ajax depending on id's of the elements
+ * and fills other depending elements with content.
+ * @param {type} what
+ * @returns {undefined}
+ */
+function fill(what) {
+    if(what == '#definition_id')
+    {
+        var id = $('select#definition_id option:selected').val();
+        if(id)
+        {
+           $.get('/pms/entity-definition/preview/' + id, function(data) {
+                $('.attribute-content').html(data); 
+            }); 
+        }        
+    }
+    if(what == '#client_id')
+    {
+        var id = $('select#client_id option:selected').val();
+        if(id) 
+        {
+            $.get('/pms/client/preview/' + id, function(data) {
+                $('.client-content').html(data); 
+            });    
+        }        
+    }
+}
+
