555449d2c1ac5ecb32f930f4fc69702cfac12fb8
Sinisa Ristic <sinisa.ristic@gmail.com>
2015-12-03 08:35:28 +0100

    Namestena putanja do prve strane i ubacen logout link.

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -23,6 +23,17 @@ return array(
                     ),
                 ),
             ),
+            'logout' => array(
+                'type' => 'Zend\Mvc\Router\Http\Literal',
+                'options' => array(
+                    'route'    => '/pms/index/logout',
+                    'defaults' => array(
+//                        'controller' => 'Application\Controller\Index',
+                        'controller' => 'Pms\Controller\Index',
+                        'action'     => 'index',
+                    ),
+                ),
+            ),
             // The following is a route to simplify getting started creating
             // new controllers and actions without needing to create a new
             // module. Simply drop new controllers in, and you can access them
