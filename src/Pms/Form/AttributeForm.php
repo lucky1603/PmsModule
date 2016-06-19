52ec6aa4d7b5cd5fd42c00472dac47e0e1aa8086
lucky1603 <sinisa.ristic@gmail.com>
2016-06-19 15:58:20 +0200

    changes in attribute definition

--- src/Pms/Form/AttributeForm.php
+++ src/Pms/Form/AttributeForm.php
@@ -10,10 +10,16 @@ class AttributeForm extends Form
         parent::__construct('AttributeForm');
         $this->setAttribute('method', 'post');
         $this->setAttribute('enctype', 'multipart/form-data');
+        $this->setAttribute('class', 'form-horizontal');
+        $this->setAttribute('role', 'form');
+
         
         // Initialize options field.
         $select = new \Zend\Form\Element\Select('type');
         $select->setLabel('Data type');    
+        $select->setLabelAttributes([
+            'class' => 'control-label col-xs-2',
+        ]);
         $select->setValueOptions([
             'boolean' => 'BOOLEAN',
             'character' => 'VARCHAR',
@@ -23,28 +29,35 @@ class AttributeForm extends Form
             'timestamp' => 'TIME',
             'select' => 'SELECT',
         ]);        
-        $select->setAttribute('class', 'attr-type');
+        $select->setAttribute('class', 'attr-type form-control');
         $this->add($select);
         
         // Initialize scope field.
         $scope = new \Zend\Form\Element\Select('scope');
         $scope->setLabel('Accessibility');    
+        $scope->setLabelAttributes([
+            'class' => 'control-label col-xs-2',
+        ]);
         $scope->setValueOptions([
             0 => 'ALL',
             1 => 'DEFINITION',
             2 => 'OBJECT',
         ]);        
-        $scope->setAttribute('class', 'scope');
+        $scope->setAttribute('class', 'scope form-control');
         $this->add($scope);
         
         $this->add([
             'name' => 'code',
             'attributes' => [
                 'type' => 'text',
-                'required' => 'required'
+                'required' => 'required',
+                'class' => 'form-control',
             ],
             'options' => [
                 'label' => 'Code',
+                'label_attributes' => [
+                    'class' => 'control-label col-xs-2',
+                ],
             ],
         ]);
         
@@ -52,10 +65,14 @@ class AttributeForm extends Form
             'name' => 'label',
             'attributes' => [
                 'type' => 'text',
-                'required' => 'required'
+                'required' => 'required',
+				'class' => 'form-control',
             ],
             'options' => [
                 'label' => 'Label',
+				'label_attributes' => [
+					'class' => 'control-label col-xs-2',
+				],
             ],
         ]);
         
@@ -63,9 +80,13 @@ class AttributeForm extends Form
             'name' => 'sort_order',
             'attributes' => [
                 'type' => 'text',
+				'class' => 'form-control',
             ],
             'options' => [
                 'label' => 'Sort order',
+				'label_attributes' => [
+					'class' => 'control-label col-xs-2',
+				],
             ],
         ]);
         
@@ -73,28 +94,36 @@ class AttributeForm extends Form
             'name' => 'unit',
             'attributes' => [
                 'type' => 'text',
+				'class' => 'form-control',
             ],
             'options' => [
                 'label' => 'Unit',
+				'label_attributes' => [
+					'class' => 'control-label col-xs-2',
+				],
             ],
         ]);
         
         // Initialize options field.
         $unique = new \Zend\Form\Element\Select('unique');
         $unique->setLabel('Unique');    
+        $unique->setLabelAttributes(['class' => 'control-label col-xs-2']);
         $unique->setValueOptions([
             false => 'FALSE',
             true => 'TRUE',
         ]);        
+	$unique->setAttribute('class', 'form-control');
         $this->add($unique);
         
         // Initialize options field.
         $nullable = new \Zend\Form\Element\Select('nullable');
         $nullable->setLabel('Can be null');    
+        $nullable->setLabelAttributes(['class' => 'control-label col-xs-2']);
         $nullable->setValueOptions([
             false => 'FALSE',
             true => 'TRUE',
         ]);        
+	$nullable->setAttribute('class', 'form-control');
         $this->add($nullable);
         
         // Hidden field for the option count
@@ -111,7 +140,8 @@ class AttributeForm extends Form
             'attributes' => [
                 'type' => 'Submit', 
                 'value' => 'Save',
-                'id' => 'submitbutton'
+                'id' => 'submitbutton',
+				'class' => 'ui-button form-control',
             ],
         ]);
     }
